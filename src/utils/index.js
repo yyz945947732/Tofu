5436089e31afb2fae3310291d7cb03c15f9f43a1
yyz <945947732@qq.com>
2020-02-17 19:35:14 +0800

    Utils

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,19 +1,13 @@
-const getLegalNum = function getLegalNum(num) {
-  return typeof num === 'number' ? Math.floor(num < 1 ? 1 : num) : 0;
-};
+const getLegalNum = (num) => (typeof num === 'number' ? Math.floor(num < 1 ? 1 : num) : 0);
 
 const defineProperty = function defineProperty(obj, prop, value) {
   const legal = typeof prop === 'string' && typeof value !== 'undefined';
   if (legal) Object.defineProperty(obj, prop, { value, writable: true, configurable: true });
 };
 
-const isObject = function isObject(arg) {
-  return typeof arg === 'object' && arg !== null && !(arg instanceof Array);
-};
+const isObject = (arg) => typeof arg === 'object' && arg !== null && !(arg instanceof Array);
 
-const random = function random(arr) {
-  return arr instanceof Array ? arr[Math.floor(Math.random() * arr.length)] : null;
-};
+const random = (arr) => (arr instanceof Array ? arr[Math.floor(Math.random() * arr.length)] : null);
 
 const has = (obj, prop) => (typeof Reflect !== 'undefined' && Reflect.has ? Reflect.has(obj, prop) : obj[prop]);
 
