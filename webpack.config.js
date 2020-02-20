ac84ee1cbc41e06b6a978e1132bc5c29a0fd551a
yyz <945947732@qq.com>
2020-02-20 19:11:33 +0800

    webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,16 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'Tofu.js',
+    libraryTarget: 'commonjs2',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
   },
   devtool: 'source-map',
 };
