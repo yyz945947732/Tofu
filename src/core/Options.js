8b190cff1feae45c8fe97d695190e80196a553eb
yyz <945947732@qq.com>
2020-02-21 12:37:12 +0800

    update Options

--- src/core/Options.js
+++ src/core/Options.js
@@ -71,7 +71,7 @@ const __definePropName = function __definePropName(propName) {
 
 const __toArray = function __toArray(obj, length) {
   utils.defineProperty(obj, 'length', length);
-  Object.setPrototypeOf(Object.getPrototypeOf(obj), Object.create(Array.prototype));
+  Object.setPrototypeOf(obj, Array.prototype);
 };
 
 class Options {
